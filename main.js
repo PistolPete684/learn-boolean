6307deeb1fa6be408be96cba9f5e0ff35172fa92
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-06-26 12:06:17 -0300

    adding js file

---
+++ main.js
@@ -0,0 +1,9 @@
+function trueOrFalse(val){
+  if (!val)    return "false";             
+  else     return "true";
+}
+
+// code wars answer
+// function trueOrFalse(val){
+//   return Boolean(val).toString();
+// }
